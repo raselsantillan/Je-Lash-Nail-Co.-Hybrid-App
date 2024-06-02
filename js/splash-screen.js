fb64a25539a4925bf5805a99ddafb121206d75c4
Rasel Santillan <118993780+raselsantillan@users.noreply.github.com>
2024-06-03 04:46:26 +0800

    Add files via upload

--- js/splash-screen.js
+++ js/splash-screen.js
@@ -1,11 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     var splashScreen = document.querySelector('.splash-screen');
 
-    // Show splash screen on page load
-    splashScreen.style.opacity = 1;
-
     splashScreen.addEventListener('animationend', function() {
-        // Redirect to index.html after fade-in animation
-        window.location.href = 'index.html';
+        window.location.href = 'home.html';
     });
 });
