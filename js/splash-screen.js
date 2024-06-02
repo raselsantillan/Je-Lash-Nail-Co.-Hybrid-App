e96a2ffc2581291df3798a300a3faf7158c25de7
Rasel Santillan <118993780+raselsantillan@users.noreply.github.com>
2024-06-03 05:22:01 +0800

    Delete js/splash-screen.js

--- js/splash-screen.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var splashScreen = document.querySelector('.splash-screen');
-
-    splashScreen.addEventListener('animationend', function() {
-        window.location.href = 'home.html';
-    });
-});
